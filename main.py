35bf35e604b9384f8fb16bfa137b8166b2578f51
amani <amanie4003@gmail.com>
2024-12-18 14:04:27 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the BMI calculator")
+height= float(input("What is your height (in meters)?\n"))
+weight = float(input("What is your weight (in kilograms)?\n"))
+bmi = weight/ (height ** 2)
+print(f"Your BMI is: {bmi:.2f}")
